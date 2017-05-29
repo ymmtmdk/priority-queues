c78fc28b23f6557b85ca0d5b2aa243948b9fd90a
ymmtmdk <ymmtmdk@gmail.com>
2017-05-30 08:39:41 +0900

    no comment

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # CP=lib/algs4.jar:build/classes/main:src/main/java
 CP=lib/algs4.jar:src/main/java
-java -Xrunhprof:cpu=times,file=cpu_times.txt -cp "$CP" Solver ./8puzzle/puzzle04.txt
+# java -Xrunhprof:cpu=times,file=cpu_times.txt -cp "$CP" Solver ./8puzzle/puzzle04.txt
+java -cp "$CP" -Xverify:none Solver ./8puzzle/puzzle4x4-31.txt
